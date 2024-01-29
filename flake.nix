90030223b845ac37c30f8dcf96d448e9a664da07
Peter A. <ink.splatters@pm.me>
2024-01-29 16:18:36 +0100

    * cmake * ninja * gnumake * meson * xmake * llvm tools

--- flake.nix
+++ flake.nix
@@ -1,47 +1,62 @@
 {
   description = "basic cpp development shell";
 
-
   outputs = { nixpkgs, flake-utils, self, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnsupportedSystem = true;
+        };
         hardeningDisable = [ "all" ];
 
-        shellHook = ''
-          export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
-        '';
 
         CFLAGS = "-mcpu native";
         CXXFLAGS = "${CFLAGS} -stdlib=libc++";
-        mkShell =
+        mkSh = args:
           let
-            inherit (pkgs) mkShell llvmPackages_latest;
+            inherit (pkgs) llvmPackages_latest;
           in
-          mkShell.override {
-            inherit (llvmPackages_latest) stdenv;
-          };
+          pkgs.mkShell.override
+            {
+              inherit (llvmPackages_latest) stdenv;
+            }
+            {
+
+              inherit CFLAGS CXXFLAGS;
+              nativeBuildInputs = with pkgs;[
+                cmake
+                ninja
+                gnumake
+                buck2
+                meson
+                xmake
+                llvmPackages_latest.llvm
+              ];
+
+
+              shellHook = ''
+                export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
+              '';
+            } // args;
+
       in
       {
         formatter = pkgs.nixpkgs-fmt;
         devShells = {
-          default = mkShell {
-            inherit CFLAGS CXXFLAGS shellHook;
-          };
+          default = mkSh { };
 
-          O3 = mkShell {
-            inherit shellHook;
-
+          O3 = mkSh {
             CFLAGS = "${CFLAGS} -O3";
             CXXFLAGS = "${CXXFLAGS} -O3";
           };
 
-          unhardened = mkShell {
-            inherit CFLAGS CXXFLAGS shellHook hardeningDisable;
+          unhardened = mkSh {
+            inherit hardeningDisable;
           };
 
-          O3-unhardened = mkShell {
-            inherit shellHook hardeningDisable;
+          O3-unhardened = mkSh {
+            inherit hardeningDisable;
 
             CFLAGS = "${CFLAGS} -O3";
             CXXFLAGS = "${CXXFLAGS} -O3";

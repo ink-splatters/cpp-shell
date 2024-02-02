b228faa7b92635986311ea19f5e8691595dfe71d
Peter A. <ink.splatters@pm.me>
2024-02-02 03:41:45 +0100

    * refactoring * -fuse-ld=lld

--- flake.nix
+++ flake.nix
@@ -9,37 +9,30 @@
           config.allowUnsupportedSystem = true;
         };
         hardeningDisable = [ "all" ];
-        CFLAGS = "-O3";
-        CXXFLAGS = "${CFLAGS}";
+        CFLAGS = "-fuse-ld=lld";
+        CXXFLAGS = "${CFLAGS} -stdlib=libc++";
 
-        mkSh = args:
-          let
-            inherit (pkgs) llvmPackages_latest;
-          in
-          with pkgs; mkShell.override
-            {
-              inherit (llvmPackages_latest) stdenv;
-            }
-            {
+        mkSh = args: with pkgs; mkShell.override { inherit (llvmPackages_latest) stdenv; }
+          {
+            inherit CFLAGS CXXFLAGS;
 
-              inherit CFLAGS CXXFLAGS;
-              buildInputs = [
-                cmake
-                ninja
-                gnumake
-                buck2
-                meson
-                flex
-                bison
-                ccls
-                ccache
-                (with llvmPackages_latest; [ llvm lldb clang-tools ])
-              ];
+            nativeBuildInputs = [
+              cmake
+              ninja
+              gnumake
+              buck2
+              meson
+              flex
+              bison
+              ccls
+              ccache
+              (with llvmPackages_latest; [ llvm lldb clang-tools lld ])
+            ];
 
-              shellHook = ''
-                export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
-              '';
-            } // args;
+            shellHook = ''
+              export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
+            '';
+          } // args;
 
       in
       with pkgs; {
@@ -48,7 +41,8 @@
           default = mkSh { };
 
           O3 = mkSh {
-            inherit CFLAGS CXXFLAGS;
+            CFLAGS = "${CFLAGS} -O3";
+            CXXFLAGS = "${CXXFLAGS} -O3";
           };
 
           unhardened = mkSh {
@@ -56,24 +50,30 @@
           };
 
           O3-unhardened = mkSh {
-            inherit hardeningDisable CFLAGS CXXFLAGS;
+            inherit hardeningDisable;
+            CFLAGS = "${CFLAGS} -O3";
+            CXXFLAGS = "${CXXFLAGS} -O3";
           };
         };
 
-        checks.default = stdenv.mkDerivation {
-          inherit (self.devShells.${system}.default);
+        checks.default =
+          let
+            inherit (self.devShells.${system}.default) stdenv nativeBuildInputs CFLAGS CXXFLAGS;
+          in
+          stdenv.mkDerivation {
 
-          name = "check";
-          src = ./checks;
-          dontBuild = true;
-          doCheck = true;
 
-          checkPhase = ''
-            clang++ main.cpp -o helloworld
-          '';
-          installPhase = ''
-            mkdir "$out"
-          '';
-        };
+            name = "check";
+            src = ./checks;
+            dontBuild = true;
+            doCheck = true;
+
+            checkPhase = ''
+              clang++ main.cpp -o helloworld
+            '';
+            installPhase = ''
+              mkdir "$out"
+            '';
+          };
       });
 }

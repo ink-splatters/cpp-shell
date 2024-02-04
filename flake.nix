f47d33c5cdf559a7e3475af427cb5890eec536ab
Peter A. <ink.splatters@pm.me>
2024-02-04 01:13:13 +0100

    * separated between build-time and non-build-time deps, with later being   exposed for profile-wide setup * rolled back from llvmPackages_latest.stdenv as it's broken currently

--- flake.nix
+++ flake.nix
@@ -8,13 +8,12 @@
           inherit system;
           config.allowUnsupportedSystem = true;
         };
+
         hardeningDisable = [ "all" ];
-        CFLAGS = "-fuse-ld=lld";
-        CXXFLAGS = "${CFLAGS} -stdlib=libc++";
 
         mkSh = args: with pkgs; mkShell
           {
-            inherit CFLAGS CXXFLAGS;
+            LDFLAGS = "-fused-ld=lld";
 
             nativeBuildInputs = [
               cmake
@@ -23,10 +22,9 @@
               meson
               flex
               bison
-              ccls
               ccache
-              (with llvmPackages; [ llvm lldb clang-tools lld libcxx libcxxabi binutils ])
             ];
+
 
             shellHook = ''
               export PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\] "
@@ -35,33 +33,12 @@
 
       in
       with pkgs; {
-        formatter = nixpkgs-fmt;
-        devShells = {
-          default = mkSh { };
-
-          O3 = mkSh {
-            CFLAGS = "${CFLAGS} -O3";
-            CXXFLAGS = "${CXXFLAGS} -O3";
-          };
-
-          unhardened = mkSh {
-            inherit hardeningDisable;
-          };
-
-          O3-unhardened = mkSh {
-            inherit hardeningDisable;
-            CFLAGS = "${CFLAGS} -O3";
-            CXXFLAGS = "${CXXFLAGS} -O3";
-          };
-        };
 
         checks.default =
           let
-            inherit (self.devShells.${system}.default) stdenv nativeBuildInputs CFLAGS CXXFLAGS;
+            inherit (self.devShells.${system}.default) stdenv nativeBuildInputs LDFLAGS;
           in
           stdenv.mkDerivation {
-
-
             name = "check";
             src = ./checks;
             dontBuild = true;
@@ -74,5 +51,36 @@
               mkdir "$out"
             '';
           };
+        formatter = nixpkgs-fmt;
+        devShells = {
+          default = mkSh { };
+
+          O3 = mkSh {
+            CFLAGS = "-O3";
+            CXXFLAGS = "-O3";
+          };
+
+          unhardened = mkSh {
+            inherit hardeningDisable;
+          };
+
+          O3-unhardened = mkSh {
+            inherit hardeningDisable;
+            CFLAGS = "-O3";
+            CXXFLAGS = "-O3";
+          };
+        };
+        packages.default = buildEnv {
+          name = "cpp-env";
+
+          paths = with llvmPackages; [
+            # ccls
+            lldb
+            llvm
+            clang-tools
+
+          ];
+        };
+
       });
 }

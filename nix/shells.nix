47777e04e51d1171d6e704fdd2061d20ca9ec8f2
Peter A. <ink.splatters@pm.me>
2024-02-11 12:18:05 +0100

    re-enabled pre-commit hook in default shell

--- nix/shells.nix
+++ nix/shells.nix
@@ -5,7 +5,7 @@ with pkgs; rec {
 
     name = "cpp-shell";
 
-    shellHook = ''
+    shellHook = self.checks.${system}.pre-commit-check.shellHook + ''
       export PS1="\n\[\033[01;36m\]‹⊂˖˖› \\$ \[\033[00m\]"
       echo -e "\nto install pre-commit hooks:\n\x1b[1;37mnix develop .#install-hooks\x1b[00m"
     '';

6014c8122555ceeb388d6c5214252605c4f4eedb
Peter A. <ink.splatters@pm.me>
2024-02-23 05:18:30 +0100

    libcxx

--- nix/common.nix
+++ nix/common.nix
@@ -1,8 +1,11 @@
-{ pkgs, lib, system, ... }: rec {
+{ pkgs, lib, system, ... }:
+let inherit (pkgs.llvmPackages_17) bintools libcxx;
+in with pkgs; rec {
   CFLAGS =
-    lib.optionalString ("${system}" == "aarch64-darwin") "-mcpu=apple-m1";
+    lib.optionalString ("${system}" == "aarch64-darwin") "-mcpu=apple-m1 "
+    + "-I${lib.getDev libcxx}/include/c++/v1";
   CXXFLAGS = "${CFLAGS}";
-  LDFLAGS = "-fuse-ld=lld";
+  LDFLAGS = "-fuse-ld=lld -L${libcxx}/lib";
 
   nativeBuildInputs = with pkgs; [
     bison
@@ -14,6 +17,6 @@
     meson
     ninja
     lldb_17
-    llvmPackages_17.bintools # https://matklad.github.io/2022/03/14/rpath-or-why-lld-doesnt-work-on-nixos.html
+    bintools # https://matklad.github.io/2022/03/14/rpath-or-why-lld-doesnt-work-on-nixos.html
   ];
 }

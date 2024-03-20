a5c4ddb7ce1a17015896feec63ba09ba7cc7cdc4
Peter A. <ink.splatters@pm.me>
2024-03-20 15:14:42 +0100

    updated markdownlint hook

--- nix/checks.nix
+++ nix/checks.nix
@@ -6,13 +6,15 @@ pre-commit-hooks.lib.${system}.run {
     clang-format.enable = true;
     clang-tidy.enable = true;
     deadnix.enable = true;
-    markdownlint.enable = true;
+    markdownlint = {
+      enable = true;
+      settings.config = { MD041.level = 2; };
+    };
+
     nil.enable = true;
     nixfmt.enable = true;
     statix.enable = true;
   };
-
-  settings.markdownlint.config = { MD041.level = 2; };
 
   tools = pkgs;
 }

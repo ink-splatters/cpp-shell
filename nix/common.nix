32a37fd8d3e784f512f6352c08506b94a38b4ad9
Peter A. <ink.splatters@pm.me>
2024-03-27 04:13:41 +0100

    no libcxxabi

--- nix/common.nix
+++ nix/common.nix
@@ -1,11 +1,11 @@
 { llvmPackages_17, lib, pkgs, system, ... }:
-let inherit (llvmPackages_17) bintools libcxx libcxxabi;
+let inherit (llvmPackages_17) bintools libcxx;
 in rec {
   CFLAGS =
     lib.optionalString ("${system}" == "aarch64-darwin") "-mcpu=apple-m1 ";
   CXXFLAGS = "${CFLAGS}";
   LDFLAGS = "-fuse-ld=lld -lc++ -lc++abi";
 
-  buildInputs = [ libcxx libcxxabi bintools ];
+  buildInputs = [ libcxx bintools ];
   nativeBuildInputs = with pkgs; [ ccache cmake gnumake ninja ];
 }
